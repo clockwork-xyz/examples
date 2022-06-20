357d732c006c7f74ab18668662fe5589afb969da
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-06-20 13:38:58 -0400

    added auto_withdraw

--- token_transfer/programs/token_transfer/src/lib.rs
+++ token_transfer/programs/token_transfer/src/lib.rs
@@ -13,21 +13,22 @@ use instructions::*;
 pub mod token_transfer {
     use super::*;
 
-    /**
-     * amount: u64 - deposit from sender
-     * transfer_rate: u64 - rate at which deposit is sent to receiver from escrow
-     */
+    pub fn initialize<'info>(ctx: Context<'_, '_, '_, 'info, Initialize<'info>>) -> Result<()> {
+        initialize::handler(ctx)
+    }
 
-    pub fn create_payment<'info>(
-        ctx: Context<'_, '_, '_, 'info, CreatePayment<'info>>,
+    pub fn deposit<'info>(
+        ctx: Context<'_, '_, '_, 'info, Deposit<'info>>,
         amount: u64,
         transfer_rate: u64,
     ) -> Result<()> {
-        create_payment::handler(ctx, amount, transfer_rate)
+        deposit::handler(ctx, amount, transfer_rate)
     }
 
-    pub fn initialize<'info>(ctx: Context<'_, '_, '_, 'info, Initialize<'info>>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn auto_withdraw<'info>(
+        ctx: Context<'_, '_, '_, 'info, AutoWithdraw<'info>>,
+    ) -> Result<()> {
+        auto_withdraw::handler(ctx)
     }
 
     pub fn disburse_payment(ctx: Context<'_, '_, '_, '_, DisbursePayment<'_>>) -> Result<()> {
