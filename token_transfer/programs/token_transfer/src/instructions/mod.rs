528bf824900ac039f60a4d1c222251720ec8b08a
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-06-20 14:34:12 -0400

    misc

--- token_transfer/programs/token_transfer/src/instructions/mod.rs
+++ token_transfer/programs/token_transfer/src/instructions/mod.rs
@@ -1,9 +1,11 @@
-pub mod auto_withdraw;
+pub mod auto_disburse;
+pub mod create;
 pub mod deposit;
 pub mod disburse_payment;
 pub mod initialize;
 
-pub use auto_withdraw::*;
+pub use auto_disburse::*;
+pub use create::*;
 pub use deposit::*;
 pub use disburse_payment::*;
 pub use initialize::*;
