7b3f79449b5bc46518c7b7511aa1d23f5a8a5fe7
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-06-20 13:36:01 -0400

    added new_with_signer

--- token_transfer/programs/token_transfer/src/instructions/disburse_payment.rs
+++ token_transfer/programs/token_transfer/src/instructions/disburse_payment.rs
@@ -1,18 +1,27 @@
 use {
     crate::state::*,
     anchor_lang::prelude::*,
-    anchor_spl::{token::{self, Mint, TokenAccount, Transfer}},
+    anchor_spl::{ 
+        associated_token::AssociatedToken,
+        token::{self, Mint, TokenAccount, Transfer}
+    },
     cronos_scheduler::state::Manager,
 };
 
 #[derive(Accounts)]
 pub struct DisbursePayment<'info> {
+    #[account(address = anchor_spl::associated_token::ID)]
+    pub associated_token_program: Program<'info, AssociatedToken>,
+
     #[account(seeds = [SEED_AUTHORITY], bump, has_one = manager)]
     pub authority: Box<Account<'info, Authority>>,
 
     #[account(
+        seeds = [SEED_ESCROW, sender.key().as_ref(), recipient.key().as_ref()],
+        bump,
         has_one = sender,
         has_one = recipient,
+        has_one = mint,
     )]
     pub escrow: Account<'info, Escrow>,
 
@@ -25,8 +34,8 @@ pub struct DisbursePayment<'info> {
     #[account()]
     pub recipient: AccountInfo<'info>,
 
-    #[account(
-        mut, 
+    #[account( 
+        mut,
         associated_token::authority = recipient,
         associated_token::mint = mint,
     )]
@@ -48,21 +57,22 @@ pub struct DisbursePayment<'info> {
 
 pub fn handler(ctx: Context<'_, '_, '_, '_, DisbursePayment<'_>>) -> Result<()> {
     // Get accounts
-    let escrow = &mut ctx.accounts.escrow;
-    let recipient_token_account = &mut ctx.accounts.recipient_token_account;
+    let escrow = &ctx.accounts.escrow;
+    let vault = &ctx.accounts.vault;
+    let recipient_token_account = &ctx.accounts.recipient_token_account;
     let token_program = &ctx.accounts.token_program;
-    let vault = &mut ctx.accounts.vault;
+
+    let bump = *ctx.bumps.get("escrow").unwrap();
 
     // transfer from vault to recipient's token account
     token::transfer(
-        CpiContext::new(
-            token_program.to_account_info(),
+        CpiContext::new_with_signer(
+            token_program.to_account_info(), 
             Transfer {
                 from: vault.to_account_info().clone(),
                 to: recipient_token_account.to_account_info().clone(),
                 authority: escrow.to_account_info().clone(),
-            },
-        ),
+            }, &[&[SEED_ESCROW, &escrow.sender.to_bytes(), &escrow.recipient.to_bytes(), &[bump]]]),
         escrow.transfer_rate,
     )?;
 
