5b263e2842283d225b173420615748ccc657c879
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-10-06 15:30:05 -0500

    use client to invoke queue_create

--- hello_clockwork/programs/hello_clockwork/src/instructions/hello_world.rs
+++ hello_clockwork/programs/hello_clockwork/src/instructions/hello_world.rs
@@ -5,17 +5,15 @@ use {
 #[derive(Accounts)]
 #[instruction(name: String)]
 pub struct HelloWorld<'info> {
-    #[account(
-        address = hello_queue.pubkey(),
-        signer, 
-    )]
+    #[account(address = hello_queue.pubkey(), signer)]
     pub hello_queue: Account<'info, Queue>,
 }
 
 pub fn handler(_ctx: Context<HelloWorld>, name: String) -> Result<CrankResponse> {
     msg!(
         "Hello {}! The current time is: {}",
-        name,Clock::get().unwrap().unix_timestamp
+        name,
+        Clock::get().unwrap().unix_timestamp
     );
 
     Ok(CrankResponse {
