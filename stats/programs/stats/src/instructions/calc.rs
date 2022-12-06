93a9afa761b267f0e3149e49d4ce92aca6aba909
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-12-06 12:54:51 -0600

    print data after it has been updated

--- stats/programs/stats/src/instructions/calc.rs
+++ stats/programs/stats/src/instructions/calc.rs
@@ -44,11 +44,24 @@ pub fn handler<'info>(ctx: Context<Calc<'info>>) -> Result<()> {
 
             stat.twap(Price { price: price.price, timestamp: price.publish_time })?;
 
+            msg!("stats account array size - N = 5");
+            msg!("ts: {}, price: {}", stat.price_history[0].timestamp, stat.price_history[0].price);
+            msg!("ts: {}, price: {}", stat.price_history[1].timestamp, stat.price_history[1].price);
+            msg!("ts: {}, price: {}", stat.price_history[2].timestamp, stat.price_history[2].price);
+            msg!("ts: {}, price: {}", stat.price_history[3].timestamp, stat.price_history[3].price);
+            msg!("ts: {}, price: {}", stat.price_history[4].timestamp, stat.price_history[4].price);
+
+            let oldest_price = stat.price_history[Stat::index_of(stat.tail as u64)];
+            let newest_price = stat.price_history[Stat::index_of(stat.head as u64)];
+
             msg!("------------LIVE DATA------------");
             msg!("     live price: {}", price.price);
             msg!("      live time: {}", price.publish_time);
             msg!("--------STATS ACCOUNT DATA-------");
             msg!("     price feed: {}", stat.price_feed);
+            msg!("      authority: {}", stat.authority);
+            msg!("     oldest - ts: {}, price: {}", oldest_price.timestamp, oldest_price.price);
+            msg!("     newest - ts: {}, price: {}", newest_price.timestamp, newest_price.price);
             msg!("      authority: {}", stat.authority);
             msg!("      TWA Price: {}", stat.twap);
             msg!(" lookback window: {} seconds", stat.lookback_window);
