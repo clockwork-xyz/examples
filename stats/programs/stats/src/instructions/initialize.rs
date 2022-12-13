4330c163caf202dbd2f1ed692fa3ce61ef4ad458
Nick Garfield <nick@clockwork.xyz>
2022-12-13 11:56:55 -0600

    Use zero-copy ring buffer to store price data (#37)
    * Use zero-copy ring buffer to store price data
    
    * Nullify data samples older than the lookback window

--- stats/programs/stats/src/instructions/initialize.rs
+++ stats/programs/stats/src/instructions/initialize.rs
@@ -3,6 +3,9 @@ use {
     anchor_lang::{prelude::*, solana_program::system_program},
     std::mem::size_of,
 };
+
+static INITIAL_BUFFER_LIMIT: usize = 5;
+
 #[derive(Accounts)]
 #[instruction(lookback_window: i64, sample_rate: i64, id: String)]
 pub struct Initialize<'info> {
@@ -15,13 +18,13 @@ pub struct Initialize<'info> {
             SEED_STAT, 
             price_feed.key().as_ref(), 
             signer.key().as_ref(),
-            id.as_bytes()
+            &lookback_window.to_le_bytes(),
         ],
         bump,
         payer = signer,
-        space = 8 + size_of::<Stat>(),
+        space = 8 + size_of::<Stat>() + (INITIAL_BUFFER_LIMIT * size_of::<crate::PriceData>()),
     )]
-    pub stat: Account<'info, Stat>,
+    pub stat: AccountLoader<'info, Stat>,
 
     #[account(mut)]
     pub signer: Signer<'info>,
@@ -30,12 +33,12 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn handler<'info>(ctx: Context<Initialize<'info>>, lookback_window: i64, sample_rate: i64, id: String) -> Result<()> {
+pub fn handler<'info>(ctx: Context<Initialize<'info>>, lookback_window: i64, sample_rate: i64) -> Result<()> {
     let price_feed = &ctx.accounts.price_feed;
     let signer = &ctx.accounts.signer;
-    let stat = &mut ctx.accounts.stat;
+    let stat = &mut ctx.accounts.stat.load_init()?;
 
-    stat.new(price_feed.key(), signer.key(), lookback_window, sample_rate, id)?;
+    stat.new(price_feed.key(), signer.key(), lookback_window, sample_rate, INITIAL_BUFFER_LIMIT)?;
 
     Ok(())
 }
--- stats/programs/stats/src/state/stat.rs
+++ stats/programs/stats/src/state/stat.rs
@@ -1,7 +1,4 @@
-use {
-    anchor_lang::{prelude::*, AnchorDeserialize},
-    std::collections::VecDeque,
-};
+use anchor_lang::prelude::*;
 
 pub const SEED_STAT: &[u8] = b"stat";
 
@@ -9,28 +6,27 @@ pub const SEED_STAT: &[u8] = b"stat";
  * Stat
  */
 
-#[account]
-#[derive(Debug)]
+#[account(zero_copy)]
 pub struct Stat {
     pub price_feed: Pubkey,
     pub authority: Pubkey,
-    pub price_history: VecDeque<(i64, i64)>,
     pub lookback_window: i64,
-    pub sample_count: i64,
+    pub sample_count: usize,
     pub sample_sum: i64,
     pub sample_rate: i64,
-    pub twap: i64,
-    pub id: String,
+    pub sample_avg: i64,
+    pub buffer_limit: usize,
+    pub head: usize,
 }
 
 impl Stat {
-    pub fn pubkey(price_feed: Pubkey, authority: Pubkey, id: String) -> Pubkey {
+    pub fn pubkey(price_feed: Pubkey, authority: Pubkey, lookback_window: i64) -> Pubkey {
         Pubkey::find_program_address(
             &[
                 SEED_STAT,
                 price_feed.as_ref(),
                 authority.as_ref(),
-                id.as_bytes(),
+                &lookback_window.to_le_bytes(),
             ],
             &crate::ID,
         )
@@ -49,85 +45,24 @@ impl TryFrom<Vec<u8>> for Stat {
  * StatAccount
  */
 
-pub trait StatAccount {
-    fn new(
+impl Stat {
+    pub fn new(
         &mut self,
         price_feed: Pubkey,
         authority: Pubkey,
         lookback_window: i64,
         sample_rate: i64,
-        id: String,
-    ) -> Result<()>;
-    fn twap(&mut self, timestamp: i64, price: i64) -> Result<()>;
-}
-
-impl StatAccount for Account<'_, Stat> {
-    fn new(
-        &mut self,
-        price_feed: Pubkey,
-        authority: Pubkey,
-        lookback_window: i64,
-        sample_rate: i64,
-        id: String,
+        buffer_limit: usize,
     ) -> Result<()> {
         self.price_feed = price_feed;
         self.authority = authority;
-        self.price_history = VecDeque::new();
         self.lookback_window = lookback_window;
         self.sample_count = 0;
         self.sample_sum = 0;
         self.sample_rate = sample_rate;
-        self.twap = 0;
-        self.id = id;
-        Ok(())
-    }
-
-    fn twap(&mut self, timestamp: i64, price: i64) -> Result<()> {
-        // always insert first encountered pricing data
-        if self.sample_count == 0 {
-            self.price_history.push_back((timestamp, price));
-            self.sample_sum = self.sample_sum.checked_add(price).unwrap();
-            self.sample_count = self.sample_count.checked_add(1).unwrap();
-        } else {
-            let newest_price = *self
-                .price_history
-                .get((self.sample_count - 1) as usize)
-                .unwrap();
-            let oldest_price = *self.price_history.get(0).unwrap();
-
-            // if the latest price is after sample rate threshhold then insert new pricing data
-            if timestamp >= newest_price.0 + self.sample_rate {
-                self.price_history.push_back((timestamp, price));
-                self.sample_sum = self.sample_sum.checked_add(price).unwrap();
-                self.sample_count = self.sample_count.checked_add(1).unwrap();
-            }
-
-            // while oldest pricing data is less lookback window then pop that element
-            while oldest_price.0
-                < Clock::get().unwrap().unix_timestamp - self.lookback_window.clone()
-            {
-                let popped_element = self.price_history.pop_front().unwrap();
-                self.sample_sum = self.sample_sum.checked_sub(popped_element.1).unwrap();
-                self.sample_count = self.sample_count.checked_sub(1).unwrap();
-            }
-
-            msg!(
-                "     oldest - ts: {}, price: {}",
-                oldest_price.0,
-                oldest_price.1
-            );
-            msg!(
-                "     newest - ts: {}, price: {}",
-                newest_price.0,
-                newest_price.1
-            );
-        }
-
-        match self.sample_sum.checked_div(self.sample_count) {
-            Some(twap) => self.twap = twap,
-            None => {}
-        }
-
+        self.sample_avg = 0;
+        self.buffer_limit = buffer_limit;
+        self.head = 0;
         Ok(())
     }
 }
