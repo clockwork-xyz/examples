24d0d4dc35e0fcbd888de6bfa3635400e5731bc1
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-12-26 16:20:43 -0600

    move PriceData struct to dataset file

--- pyth_stats/programs/pyth_stats/src/state/dataset.rs
+++ pyth_stats/programs/pyth_stats/src/state/dataset.rs
@@ -1,4 +1,8 @@
-use anchor_lang::prelude::*;
+use {
+    anchor_lang::prelude::*,
+    bytemuck::{Pod, Zeroable},
+    pyth_sdk_solana::Price,
+};
 
 pub const SEED_DATASET: &[u8] = b"dataset";
 
@@ -12,5 +16,21 @@ pub struct Dataset {}
 impl Dataset {
     pub fn pubkey(stat: Pubkey) -> Pubkey {
         Pubkey::find_program_address(&[SEED_DATASET, stat.as_ref()], &crate::ID).0
+    }
+}
+
+#[derive(Copy, Clone, Zeroable, Pod, Default)]
+#[repr(C)]
+pub struct PriceData {
+    pub price: i64,
+    pub ts: i64,
+}
+
+impl From<Price> for PriceData {
+    fn from(price: Price) -> Self {
+        PriceData {
+            price: price.price,
+            ts: price.publish_time,
+        }
     }
 }
