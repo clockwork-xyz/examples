1d81b0a5574d7abb0b98c3016b7ffff2759dcc75
mwrites <20499416+mwrites@users.noreply.github.com>
2023-02-21 02:20:36 +0800

    event stream

--- event_stream/README.md
+++ event_stream/README.md
@@ -2,17 +2,15 @@
 
 ## Devnet
 **1. Program Side - Deploying the Handler Program**
-
 We start by defining an instruction to execute, that is the __"WHAT"__:
 - We have already prepared a handler.
 - You just have to deploy it using `./deploy.sh devnet` _(nothing fancy, the usual program ids and network switch)_.
 
 **2. Client Side - Creating a Thread**
-
 Time to switch perspective, we need to do some work off-chain now, we will create a Thread, that's the __"HOW"__:
-- Navigate to the `client` directory. _We have prepared a Rust client, but you
-  can very well use any Solana client you prefer (JS, Python, etc)._
-- Run `cargo run`.
+- Check the `tests` folder, we are using anchor tests as a client.
+- Run `anchor tests`, this will create a __Thread__ that listens for a certain account and print logs whenever the
+  account is updated.
 
 ## How do I know if it works?
 Let's see how we can observe our newly created Thread:
@@ -41,17 +39,15 @@ The workflow is pretty similar with __devnet__ with the exception that you are r
 - Use the `clockwork localnet` to start a local validator with the Clockwork Engine.
 
 **1. Program Side - Deploying the Handler Program**
-
 We start by defining an instruction to execute, that is the __"WHAT"__:
 - We have already prepared a handler.
 - You just have to deploy it using `./deploy.sh localnet` _(nothing fancy, the usual program ids and network switch)_.
 
 **2. Client Side - Creating a Thread**
-
 Time to switch perspective, we need to do some work off-chain now, we will create a Thread, that's the __"HOW"__:
-- Navigate to the `client` directory. _We have prepared a Rust client, but you
-  can very well use any Solana client you prefer (JS, Python, etc)._
-- Run `cargo run --features localnet`, this will create a __Thread__ that continuously calls your program handler.
+- Check the `tests` folder, we are using anchor tests as a client.
+- Run `anchor tests`, this will create a __Thread__ that listens for a certain account and print logs whenever the 
+  account is updated.
 
 ## Common Errors
 Please refer to the [FAQ](https://github.com/clockwork-xyz/docs/blob/main/FAQ.md#common-errors).
--- event_stream/tests/event_stream.ts
+++ event_stream/tests/event_stream.ts
@@ -1,16 +1,114 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { EventStream } from "../target/types/event_stream";
+import {Program} from "@project-serum/anchor";
+import {EventStream} from "../target/types/event_stream";
+// 👇 The new import
+import {getThreadAddress, CLOCKWORK_THREAD_PROGRAM_ID} from "@clockwork-xyz/sdk";
+import {PublicKey, SystemProgram} from "@solana/web3.js";
+import {expect} from "chai";
+
+const provider = anchor.AnchorProvider.env();
+anchor.setProvider(provider);
+const program = anchor.workspace.EventStream as Program<EventStream>;
+
 
 describe("event_stream", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    it("It creates a Thread!", async () => {
+        const [event] = PublicKey.findProgramAddressSync(
+            [anchor.utils.bytes.utf8.encode("event")], // 👈 make sure it matches on the prog side
+            program.programId
+        );
 
-  const program = anchor.workspace.EventStream as Program<EventStream>;
+        await initializeThread(event);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+        // Ping an account change every 10s
+        // Ask program to update the account with address `event`
+        for (let i = 0; i < 9; i++) {
+            await sleep(10);
+            await ping(event);
+        }
+    });
 });
+
+const initializeThread = async (event: PublicKey) => {
+    // 0️⃣ Accounts
+    const threadLabel = "eventstream_21-0151";
+    const [threadAuthority] = PublicKey.findProgramAddressSync(
+        [anchor.utils.bytes.utf8.encode("authority")], // 👈 make sure it matches on the prog side
+        program.programId
+    );
+    const threadAddress = getThreadAddress(threadAuthority, threadLabel);
+
+    console.log("event:" + event.toString());
+    console.log("threadLabel:" + threadLabel.toString());
+    console.log("threadAuthority:" + threadAuthority.toString());
+    console.log("threadAddress:" + threadAddress.toString());
+
+    // 1️⃣ Ask your Program to do a CPI to create a Thread
+    try {
+        const tx = await program.methods.initialize(threadLabel)
+            .accounts({
+                systemProgram: SystemProgram.programId,
+                clockwork: CLOCKWORK_THREAD_PROGRAM_ID,
+                signer: provider.publicKey,
+                authority: threadAuthority,
+                eventThread: threadAddress,
+                event: event,
+            })
+            .rpc();
+        print_address("🤖 Program", program.programId.toString());
+        print_thread_address("🧵 Thread", threadAddress);
+        print_tx("✍️ Tx", tx);
+    } catch (e) {
+        // ❌
+        // 'Program log: Instruction: ThreadCreate',
+        //     'Program 11111111111111111111111111111111 invoke [2]',
+        //     'Allocate: account Address { address: ..., base: None } already in use'
+        //
+        // -> If you encounter this error, the thread address you are trying to assign is already in use,
+        //    you can just change the threadLabel, to generate a new address.
+        // -> OR update the thread with a ThreadUpdate instruction (more on this in another guide)
+
+
+        // ❌
+        // 'Program log: AnchorError caused by account: thread. Error Code: AccountNotSystemOwned.
+        // Error Number: 3011. Error Message: The given account is not owned by the system program.',
+        //
+        // -> Same as the above, actually
+        //    What's happening is that, the account is now owned by the ThreadProgram,
+        //    and thus the Account has been successfully created.
+        //    The account owner is now the ThreadProgram, but in your program you are expecting a SystemAccount
+        // -> What to do? -> (Same as the first error) for your tests it's fine to just use a new address.
+        console.error(e);
+        expect.fail(e);
+    }
+}
+
+const ping = async (event: PublicKey) => {
+    try {
+        const tx = await program.methods.ping()
+            .accounts({event: event})
+            .rpc()
+        print_tx("✍️ Ping Tx", tx);
+    } catch (e) {
+        console.error(e);
+        expect.fail(e);
+    }
+}
+
+function sleep(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 1000 * seconds);
+    });
+}
+
+const print_address = (label, address) => {
+    console.log(`${label}: https://explorer.solana.com/address/${address}?cluster=devnet`);
+}
+
+const print_tx = (label, address) => {
+    console.log(`${label}: https://explorer.solana.com/tx/${address}?cluster=devnet`);
+}
+
+const print_thread_address = (label, address) => {
+    console.log(`${label}: https://explorer.clockwork.xyz/address/${address}?network=devnet`);
+}
