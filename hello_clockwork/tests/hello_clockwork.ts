3616e6f3c397a43c32a8503e396b38a74acf1d33
mwrites <20499416+mwrites@users.noreply.github.com>
2023-03-05 22:24:36 +0800

    stream program logs

--- hello_clockwork/tests/hello_clockwork.ts
+++ hello_clockwork/tests/hello_clockwork.ts
@@ -1,3 +1,4 @@
+import { spawn } from "child_process";
 import { expect } from "chai";
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
@@ -51,8 +52,16 @@ describe("hello_clockwork", () => {
         anchor.web3.LAMPORTS_PER_SOL, // pre-fund amount
       );
       const [threadAddress, threadBump] = clockworkProvider.getThreadPDA(wallet.publicKey, threadId)
+      const threadAccount = await clockworkProvider.getThreadAccount(threadAddress);
+
+      console.log("ThreadAccount: ", threadAccount);
       print_address("🧵 Thread", threadAddress);
       print_tx("🖊️  ThreadCreate", tx);
+
+      const cmd = spawn("solana", ["logs", "-u", "devnet", program.programId.toString()]);
+      cmd.stdout.on("data", data => {
+          console.log(`Program Logs: ${data}`);
+      });
     } catch (e) {
       // ❌
       // 'Program log: Instruction: ThreadCreate',
@@ -66,6 +75,5 @@ describe("hello_clockwork", () => {
       expect.fail(e);
     }
   });
-
 });
 
